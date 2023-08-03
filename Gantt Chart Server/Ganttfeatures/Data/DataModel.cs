f0df137451337436416fd34a90379a8bc03d1b40
poojapriya.krishnamorthy <poojapriya.krishnamorthy@syncfusion.com>
2023-08-03 18:13:58 +0530

    updated datamodel

--- Gantt Chart Server/Ganttfeatures/Data/DataModel.cs
+++ Gantt Chart Server/Ganttfeatures/Data/DataModel.cs
@@ -26,7 +26,9 @@
             new TaskData() { ID = 11, Name = "DateOnlyTimeOnly", Url="DateOnlyTimeOnly", PID = 10 },
             new TaskData() { ID = 12, Name = "Remote data binding", PID = 1 },
             new TaskData() { ID = 13, Name = "Load child on demand", PID=12, Url="LoadChildOnDemand" },
-            new TaskData() { ID = 14, Name = "URL Adaptor", PID=12, Url="URLAdaptor" },
+            new TaskData() { ID = 14, Name = "Custom Adaptor", PID=12, Url="CustomAdaptor" },
+            new TaskData() { ID = 15, Name = "URL Adaptor", PID=12, Url="URLAdaptor" },
+            new TaskData() { ID = 16, Name = "WebAPI Adaptor", PID=12, Url="WebAPIAdaptor" },
         };
             return Tasks;
         }
